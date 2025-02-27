7e898d3ee7c37c42f60bb0190816fda719c02f33
graylobo <cazador.min@gmail.com>
2025-02-27 13:06:20 +0900

    feat: server info

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-    "http://ec2-3-35-11-25.ap-northeast-2.compute.amazonaws.com:4000/api";
+const API_URL = "http://localhost:4000/api";
 
 const api = axios.create({
     baseURL: API_URL,
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://ec2-3-35-11-25.ap-northeast-2.compute.amazonaws.com',
-      'http://ec2-3-35-11-25.ap-northeast-2.compute.amazonaws.com:3000',
-    ],
+    origin: ['http://localhost', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
